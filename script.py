52e441a886404d04b7db0283b674d27a3414c1db
murali0127 <smuralidharan845@gmail.com>
2026-03-04 23:13:11 +0530

    task-2 completed

---
+++ script.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import glob
+
+files = glob.glob('data/*.csv')
+dfs = [pd.read_csv(file) for file in files]
+
+df =  pd.concat(dfs)
+
+df = df[df['product'] == 'pink morsel']
+#create Sales Column
+df['price'] = df['price'].str.replace('$', '' ,regex=False)
+df['sales' ] = df['price'].astype(float) * df['quantity']
+
+cols = [ col for col in df.columns if col in ['sales', 'date', 'region']]
+
+print(df[cols])
+#save
+# df.to_csv('task-2_formating_data_completed.csv',index=False)
