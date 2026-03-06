694e23fa01a53ad22edeb09b2b02e7305780b3e3
murali0127 <smuralidharan845@gmail.com>
2026-03-06 19:44:36 +0530

    Task-4 completed

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,4 @@
-from dash import  Dash, html, dcc
+from dash import  Dash, html, dcc, Output, Input, callback
 import glob
 import plotly.express as px
 import pandas as pd
@@ -6,7 +6,7 @@ import pandas as pd
 # file = glob.glob('./task-2_formating_data_completed.csv')
 
 #Load CSV data
-df = pd.read_csv('task-2_formating_data_completed.csv')
+df = pd.read_csv('./data/task-2_formating_data_completed.csv')
 
 df['Date'] = pd.to_datetime(df['date'])
 
@@ -58,12 +58,71 @@ pie_fig = px.pie(
 #Initialize Dash App
 app = Dash(__name__)
 
-app.layout = html.Div(
+
+app.layout = html.Div(className='container',
       children=[html.H1(children='Hello User'),
-                html.Div(children='''Visualization of Pink Morsels Sales based on date.'''),
-                dcc.Graph(figure=fig),
-                dcc.Graph(figure=pie_fig)]
+                html.Div(className='contianer', id='title',children='''Visualization of Pink Morsels Sales based on date.'''),
+                dcc.Graph(id='total-sales',figure=fig),
+                dcc.RadioItems(
+                      id='region-filter',
+                      options = [
+                            {'label': 'North', 'value': 'north'},
+                            {'label': 'East', 'value': 'east'},
+                            {'label': 'South', 'value': 'south'},
+                            {'label': 'West', 'value': 'west'},
+                            {'label': 'All', 'value': 'all'}
+                      ],
+                      value = 'all',
+                      inline = True
+                ),
+                 html.Div(className="charts", children=[
+                       dcc.Graph(id="sales-chart"),
+                       html.Div(
+                             children=[
+                                   dcc.Graph(id="sales-pie", figure=pie_fig)                                   
+                             ]
+                             
+                       )
+                  ])
+
+                ])
+
+@app.callback(
+      Output('sales-chart', 'figure'),
+      Input('region-filter', 'value')
+
 )
+
+
+def update_chart(region):
+      if region == 'all':
+            filtered_df = df
+      else:
+            df['region'] = df['region'].str.lower()
+            filtered_df = df[df['region'] == region]
+      fig = px.line(
+            filtered_df,
+            x = 'Date',
+            y = 'sales',
+            title='Pink morsel sales' ,
+            color = 'region'
+      )
+      fig.add_shape(
+            type="line",
+            x0="2021-01-15",
+            x1="2021-01-15",
+            y0=0,
+            y1=filtered_df["sales"].max(),
+            line=dict(color="red", dash="dash")
+      )
+
+      fig.update_layout(
+            xaxis_title='Date',
+            yaxis_title='Sales',
+            template='plotly_white'
+      ) 
+      return fig
+
 
 if __name__ == '__main__':
       app.run(debug=True)
